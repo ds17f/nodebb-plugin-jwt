aedb3544070c7d7c74fcbfdf0e15bd11bedca146
Damian Silbergleith Cunniff <dcunniff@gmail.com>
2017-05-31 12:59:52 -0400

    use-cookies: Switch the plugin to emit cookies
    * Emits cookie on login
    * Kills cookie on logout
    * Exposes options in admin to set parameters for cookie

--- library.js
+++ library.js
@@ -3,8 +3,7 @@
 
     var meta = module.parent.require('./meta'),
         user = module.parent.require('./user'),
-        controllers = require('./lib/controllers'),
-        nconf = require("nconf");
+        controllers = require('./lib/controllers');
 
     var constants = Object.freeze({
         'name': "JWT",
@@ -21,35 +20,12 @@
             _ = require('lodash'),
             router = params.router,
             hostMiddleware = params.middleware,
-            hostControllers = params.controllers,
-            url=nconf.get("url");
+            hostControllers = params.controllers;
 
         router.get('/admin/plugins/jwt', hostMiddleware.admin.buildHeader, controllers.renderAdminPage);
         router.get('/api/admin/plugins/jwt', controllers.renderAdminPage);
-        
-        function getToken(req, res, next) {
-
-            if (req.user && req.user.uid) {
-                
-                meta.settings.get('jwt', function (err, settings) {
-                    user.getUserData(req.user.uid, function (err, user) {
-                        user._uid=user.uid;
-                        var token = jwt.sign(_.omit(user, 'password'), settings.secret, {
-                            expiresIn: 60 * 5
-                        });
-
-                        res.send(token);
-                    });
-                });
-            } else {
-                res.redirect('/login');
-            }
-        }
-
-        router.get('/api/jwt', getToken);
 
         JWT.reloadSettings();
-
         callback();
     };
 
@@ -69,6 +45,46 @@
         });
     };
 
+    JWT.writeCookieOnPage = function (req, res, next) {
+      var jwt = require('jsonwebtoken'),
+        _ = require('lodash');
+
+      if (req.user && req.user.uid) {
+          user.getUserData(req.user.uid, function (err, user) {
+            user._uid = user.uid;
+            var token = jwt.sign(_.omit(user, 'password'), JWT.settings.secret, {
+              expiresIn: parseInt(JWT.settings.cookielifetime)
+            });
+
+            var exp = new Date();
+            exp.setSeconds(exp.getSeconds() + parseInt(JWT.settings.cookielifetime));
+            res.cookie(JWT.settings.cookiename, token, {
+              domain: JWT.settings.cookiedomain,
+              expires: exp,
+              httpOnly: false
+            });
+
+            next();
+          });
+      } else {
+        next();
+      }
+    };
+
+    JWT.removeCookieOnLogout = function(data, callback) {
+      data.res.clearCookie(
+        JWT.settings.cookiename, {
+          domain: JWT.settings.cookiedomain,
+          expires: new Date(),
+          path: '/'
+        });
+
+      if (typeof callback === 'function') {
+        callback();
+      } else {
+        return true;
+      }
+    };
     module.exports = JWT;
 
 }(module));
--- templates/admin/plugins/jwt.tpl
+++ templates/admin/plugins/jwt.tpl
@@ -4,9 +4,32 @@
 <form class="jwt">
     <div class="form-group">
         <p>
-            Enter your application shared secret details here.
+            Enter the domain for the cookie (ie: .yoursite.com)
+        </p>
+        <input type="text" name="cookiedomain" title="Cookie Domain" class="form-control" placeholder="Cookie Domain">
+        <p class="help-block">
+            The domain to write the cookie in, usually the root domain with a leading "." so that it can be read by subdomains.
         </p>
         <br />
+        <p>
+            Enter the name of your cookie.
+        </p>
+        <input type="text" name="cookiename" title="Cookie Name" class="form-control" placeholder="Cookie Name">
+        <p class="help-block">
+            The name of the cookie that will be saved.
+        </p>
+        <br />
+        <p>
+            How long is the cookie valid (in seconds).
+        </p>
+        <input type="text" name="cookielifetime" title="Cookie Lifetime" class="form-control" placeholder="Cookie Lifetime">
+        <p class="help-block">
+            Number of seconds that the cookie will be good for.
+        </p>
+        <br />
+        <p>
+            Enter your application shared secret details here.
+        </p>
         <input type="text" name="secret" title="Shared Secret" class="form-control" placeholder="Shared Secret">
         <p class="help-block">
             This will be used to sign the JWT.
