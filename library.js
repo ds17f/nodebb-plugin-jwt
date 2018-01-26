93a60a93d2788b90bc2da85db22561dc85193969
Damian Silbergleith Cunniff <dcunniff@gmail.com>
2018-01-25 21:13:29 -0700

    use-cookies: restore the URL based JWT

--- library.js
+++ library.js
@@ -3,7 +3,8 @@
 
     var meta = module.parent.require('./meta'),
         user = module.parent.require('./user'),
-        controllers = require('./lib/controllers');
+        controllers = require('./lib/controllers'),
+        nconf = require("nconf");
 
     var constants = Object.freeze({
         'name': "JWT",
@@ -26,10 +27,32 @@
             _ = require('lodash'),
             router = params.router,
             hostMiddleware = params.middleware,
-            hostControllers = params.controllers;
+            hostControllers = params.controllers,
+            url=nconf.get("url");
 
         router.get('/admin/plugins/jwt', hostMiddleware.admin.buildHeader, controllers.renderAdminPage);
         router.get('/api/admin/plugins/jwt', controllers.renderAdminPage);
+
+        function getToken(req, res, next) {
+
+          if (req.user && req.user.uid) {
+
+            meta.settings.get('jwt', function (err, settings) {
+              user.getUserData(req.user.uid, function (err, user) {
+                user._uid=user.uid;
+                var token = jwt.sign(_.omit(user, 'password'), settings.secret, {
+                  expiresIn: 60 * 5
+                });
+
+                res.send(token);
+              });
+            });
+          } else {
+            res.redirect('/login');
+          }
+        }
+
+        router.get('/api/jwt', getToken);
 
         JWT.reloadSettings();
         callback();
